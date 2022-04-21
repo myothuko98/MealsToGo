7b9c7836542ced2e89516120271c82a13e9566e3
Myo Thu Ko <myothuko2012@gmail.com>
2022-04-21 13:41:46 +0630

    add camera and take picture and set picture

--- src/features/setting/screens/camera.screen.js
+++ src/features/setting/screens/camera.screen.js
@@ -1,22 +1,29 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useContext } from "react";
 import { Camera } from "expo-camera";
 import styled from "styled-components/native";
 import { View, TouchableOpacity } from "react-native";
 import { Text } from "../../../components/typography/text.component";
+import { FAB } from "react-native-paper";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
 `;
 
-export const CameraScreen = () => {
+export const CameraScreen = ({navigation}) => {
   const [hasPermission, setHasPermission] = useState(null);
   const cameraRef = useRef();
+
+  const {user}  = useContext(AuthenticationContext)
 
   const snap = async () => {
     if (cameraRef) {
       const photo = await cameraRef.current.takePictureAsync();
+      AsyncStorage.setItem(`${user.uid}-photo`,photo.uri);
       console.log(photo);
+      navigation.goBack();
     }
   };
 
@@ -34,12 +41,18 @@ export const CameraScreen = () => {
     return <Text>No access to camera</Text>;
   }
   return (
-    <TouchableOpacity onPress={snap}>
+    <TouchableOpacity>
       <ProfileCamera
         ref={(camera) => (cameraRef.current = camera)}
         type={Camera.Constants.Type.front}
         ratio={"16:9"}
-      />
+      >
+        <FAB
+          icon="camera"
+          style={{ position: "absolute", margin: 20, left: 0, bottom: 0 }}
+          onPress={snap}
+        />
+      </ProfileCamera>
     </TouchableOpacity>
   );
 };
--- src/features/setting/screens/setting.screen.js
+++ src/features/setting/screens/setting.screen.js
@@ -1,13 +1,14 @@
-import React, { useContext } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import styled from "styled-components/native";
 
 import { List, Avatar } from "react-native-paper";
-
+import { useFocusEffect } from "@react-navigation/native";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { Text } from "../../../components/typography/text.component";
 import { TouchableOpacity } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const SettingsItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
@@ -19,11 +20,32 @@ const AvatarContainer = styled.View`
 
 export const SettingsScreen = ({ navigation }) => {
   const { onSignout, user } = useContext(AuthenticationContext);
+  const [photo, setPhoto] = useState(null);
+
+  const getProfilePicture = async (currentUser) => {
+    const photoUri = await AsyncStorage.getItem(`${currentUser.uid}-photo`);
+    console.log("photoUri", photoUri);
+    setPhoto(photoUri);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      getProfilePicture(user);
+    }, [user])
+  );
   return (
     <SafeArea>
       <AvatarContainer>
-        <TouchableOpacity onPress={()=>navigation.navigate("Camera")}>
-          <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+        <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+          {!photo ? (
+            <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+          ) : (
+            <Avatar.Image
+              size={180}
+              source={{ uri: photo }}
+              backgroundColor="#2182BD"
+            />
+          )}
         </TouchableOpacity>
         <Spacer position="top" size="large" />
         <Text variant="label">{user.email}</Text>
